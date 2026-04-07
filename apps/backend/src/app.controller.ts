2df8201ac096afd04e70623635933b38aa2d4eee
Nurgali <nurgali.anarbek04@mail.ru>
2026-04-07 14:39:03 +0500

    fsdds

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -4,7 +4,10 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Get('health')
+getHealth() {
+  return { status: 'ok', timestamp: new Date().toISOString() };
+}
   @Get()
   getHello(): string {
     return this.appService.getHello();
